892b14711a1c5febcc8a16c8279597d529c29ff9
Zack Chapple <zackarychapple30@gmail.com>
2025-01-18 21:51:29 -0500

    adding zephyr

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,10 +1,21 @@
 import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
-
+import { withZephyr } from "zephyr-rspack-plugin";
+import { RsbuildPlugin } from "@rsbuild/core";
+const zephyrRsbuildPlugin = (): RsbuildPlugin => ({
+  name: "zephyr-rsbuild-plugin",
+  setup(api) {
+    api.modifyRspackConfig(async (config, utils) => {
+      config = await withZephyr()(config);
+    });
+  },
+  pre: ["RsbuildCorePlugin", "RsbuildHtmlPlugin", "DefinePlugin"]
+});
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'My Site',
   icon: '/rspress-icon.png',
+  ssg: false,
   logo: {
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
@@ -18,4 +29,12 @@ export default defineConfig({
       },
     ],
   },
+  builderConfig: {
+    output: {
+      copy: {
+        patterns: [{ from: "docs/public" }]
+      }
+    },
+    plugins: [zephyrRsbuildPlugin()]
+  }
 });
